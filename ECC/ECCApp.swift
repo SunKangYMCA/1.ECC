cbcafab10364d7e6af1d8d42c1078b82ddfef8f7
SunKang <coolshark248@gmail.com>
2024-01-06 18:59:13 -0700

    no message

---
+++ ECC/ECCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECCApp.swift
+//  ECC
+//
+//  Created by 강성찬 on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct ECCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
