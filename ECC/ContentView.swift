b3541d3a03cd86ffa37b56fb97d87527b78dbe67
SunKang <coolshark248@gmail.com>
2024-01-29 19:09:32 -0700

    qq

---
+++ ECC/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ECC
+//
+//  Created by 강성찬 on 2024-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hiiiiii")
+        }
+        .padding()
+    }
+}
+
+
