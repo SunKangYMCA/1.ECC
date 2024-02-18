d215c4e1a47689688c9d4bb67cfbece5a6fd0296
SunKang <coolshark248@gmail.com>
2024-02-17 19:58:10 -0700

    add main

---
+++ ECC/Scenes/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ECC
+//
+//  Created by 강성찬 on 2024-02-17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ ECC/Scenes/Main/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  ECC
+//
+//  Created by 강성찬 on 2024-02-17.
+//
+
+import Foundation
